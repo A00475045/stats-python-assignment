cb82f68c0e6a24320b1abb87151a967ce78cdc4c
Ranjit Pandey A00475045 <ranjit.parshuram.pandey@smu.ca>
2023-12-15 21:42:46 -0400

    adds the data folder for the csv, Q2 done!

--- Question-2.py
+++ Question-2.py
@@ -5,3 +5,23 @@ Created on Fri Dec 15 20:11:38 2023
 @author: RanjitPandey1605
 """
 
+import pandas as pd
+
+
+def countAllVowels(title) :
+    title = str(title)
+    title = title.lower()
+    
+    vowelCount = sum(map(title.count, ['a','e','i','o','u']))
+    return vowelCount;
+    
+    
+        
+
+
+data = pd.read_csv('./data/titles.csv')
+
+data['vowelsCount'] = data.apply(lambda row: countAllVowels(row['title']),axis = 1)
+
+print(data)
+
